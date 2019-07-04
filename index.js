dbf0d001b0efa76d764ccbac8181fc5409665049
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2019-07-04 19:37:26 +0200

    fixed export

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // exported components that can be edited by the consuming site
-export {default as Checkout} from './src/components/Checkout';
+export {default as StripeCheckout} from './src/components/Checkout';
